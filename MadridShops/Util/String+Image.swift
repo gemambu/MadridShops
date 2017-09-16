a6080bd9c8bd0ea432d801b5c62f8cb878bcae18
Gema <gemambu@gmail.com>
2017-09-16 13:25:48 +0200

    load images with nsurlsession

--- MadridShops/Util/String+Image.swift
+++ MadridShops/Util/String+Image.swift
@@ -2,7 +2,8 @@ import UIKit
 
 extension String {
     
-    func loadImage(into imageView: UIImageView) {
+    /*
+ func loadImage(into imageView: UIImageView) {
         let queue = OperationQueue()
         
         queue.addOperation {
@@ -19,6 +20,28 @@ extension String {
             
         }
     }
+ */
     
     // hacer loadImage2, con nsurlSession
+    func loadImage(into imageView: UIImageView) {
+       let session = URLSession.shared
+        if let url = URL(string: self) {
+            let task = session.dataTask(with: url) { (data: Data?, response: URLResponse?, error: Error?) in
+                
+                OperationQueue.main.addOperation {
+                    assert(Thread.current == Thread.main)
+                    if error == nil {
+                        // OK
+                        let image = UIImage(data: data as! Data)
+                        imageView.image = image
+                        
+                    } else {
+                        // Error
+                        print("Error downloading photos")
+                    }
+                }
+            }
+            task.resume()
+        }
+    }
 }
